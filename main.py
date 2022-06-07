766b4ba85ca9edd76ec44f303de6a9d95120a1b1
Yoshida Yasuhiro <yasushi.adamant@gmail.com>
2022-06-07 21:38:31 +0900

    test

--- main.py
+++ main.py
@@ -10,44 +10,8 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
-import datetime
 
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-driver = webdriver.Remote(
-    command_executor=os.environ["SELENIUM_URL"],
-    desired_capabilities=DesiredCapabilities.FIREFOX.copy()
-)
-
-
-# Y!トピックからランキングtop10を取ってくる
-def job() -> str:
-    print(f'ジョブ開始日時：{datetime.datetime.now().strftime("%Y年%m月%d日%H:%M:%S")}')
-    driver.get('https://news.yahoo.co.jp/topics')
-    driver.implicitly_wait(0.5)
-    yjnSub_list_item = []
-    yjnSub_section_title = ""
-    try:
-        yjnSub_section = driver.find_element(by=By.CLASS_NAME, value="yjnSub_section")
-        yjnSub_section_title = yjnSub_section.find_element(by=By.CLASS_NAME, value='yjnSub_section_title').text
-        yjnSub_list_item = yjnSub_section.find_elements(by=By.CLASS_NAME, value='yjnSub_list_item')
-    except NoSuchElementException as e:
-        print("そんな要素ないぞ")
-        print(e)
-    print(yjnSub_section_title)
-    for item in yjnSub_list_item:
-        rank = item.find_element(by=By.CLASS_NAME, value="yjnSub_list_rankNum").text
-        headline = item.find_element(by=By.CLASS_NAME, value="yjnSub_list_headline").text
-        link = item.find_element(by=By.TAG_NAME, value="a").get_attribute("href")
-        print(f'{rank} ヘッドライン：{headline}')
-        print(link)
-    print(f'ジョブ終了日時：{datetime.datetime.now().strftime("%Y年%m月%d日%H:%M:%S")}')
-
-    return yjnSub_list_item
-
+from scrape import get_ranking
 
 app = Flask(__name__)
 
@@ -77,7 +41,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # スクレイピング
-    result = job()
+    result = get_ranking()
     # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result))
 
---
+++ scrape.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def get_ranking():
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://news.yahoo.co.jp/topics')
+    driver.implicitly_wait(0.5)
+    yjnSub_list_item = []
+    yjnSub_section_title = ""
+    news = []
+    try:
+        yjnSub_section = driver.find_element(by=By.CLASS_NAME, value="yjnSub_section")
+        yjnSub_section_title = yjnSub_section.find_element(by=By.CLASS_NAME, value='yjnSub_section_title').text
+        yjnSub_list_item = yjnSub_section.find_elements(by=By.CLASS_NAME, value='yjnSub_list_item')
+    except NoSuchElementException as e:
+        print("そんな要素ないぞ")
+        print(e)
+    for item in yjnSub_list_item:
+        rank = item.find_element(by=By.CLASS_NAME, value="yjnSub_list_rankNum").text
+        headline = item.find_element(by=By.CLASS_NAME, value="yjnSub_list_headline").text
+        link = item.find_element(by=By.TAG_NAME, value="a").get_attribute("href")
+        news.append(headline)
+        print(f'{rank} ヘッドライン：{headline}')
+        print(link)
+
+    driver.quit()
+
+    return news
+
+
+if __name__ == '__main__':
+    news = get_ranking()
