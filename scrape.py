14542943a0907daf764c7a573013a66dcd03f6d0
Yoshida Yasuhiro <yasushi.adamant@gmail.com>
2022-06-15 00:34:16 +0900

    test

--- scrape.py
+++ scrape.py
@@ -91,14 +91,14 @@ def get_nhk_ranking():
 
 # グルメカテゴリを取ってくる
 def get_gurume_ranking():
-    # driver = webdriver.Chrome(options=options)
-    from selenium.webdriver.firefox.options import Options
-    fire_optionx = Options()
-    fire_optionx.add_argument('--headless')
-    driver = webdriver.Firefox(options=fire_optionx)
+    driver = webdriver.Chrome(options=options)
+    # from selenium.webdriver.firefox.options import Options
+    # fire_optionx = Options()
+    # fire_optionx.add_argument('--headless')
+    # driver = webdriver.Firefox(options=fire_optionx)
     print(f'ジョブ開始日時：{datetime.datetime.now().strftime("%Y年%m月%d日%H:%M:%S")}')
     driver.get('https://entabe.jp/news/sweets')
-    driver.implicitly_wait(0.5)
+    driver.implicitly_wait(3)
     headline_list = []
     rank_list = []
     link_list = []
